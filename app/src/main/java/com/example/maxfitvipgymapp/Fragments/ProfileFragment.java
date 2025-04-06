7b6ff882ec63f2a6d185c4bd19278e9e5c952e60
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-04-06 10:44:22 +0530

    profile screen done

--- app/src/main/java/com/example/maxfitvipgymapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/maxfitvipgymapp/Fragments/ProfileFragment.java
@@ -6,9 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
-import android.view.ViewGroup.LayoutParams;
 import androidx.fragment.app.Fragment;
+
 import com.example.maxfitvipgymapp.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
 
@@ -21,6 +30,7 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Setup training log grid
         GridLayout trainingLogGrid = view.findViewById(R.id.training_log_grid);
 
         int totalDays = 364;  // 52 weeks * 7 days
@@ -48,6 +58,69 @@ public class ProfileFragment extends Fragment {
             square.setBackgroundColor(getColor(trainingData[i]));
             trainingLogGrid.addView(square);
         }
+
+        // Setup momentum chart
+        LineChart chart = view.findViewById(R.id.momentum_chart);
+
+        int[] monthData = {75, 77, 92, 52, 4, 91, 87, 49, 51, 4, 26, 91};  // Mock data
+        ArrayList<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < monthData.length; i++) {
+            entries.add(new Entry(i, monthData[i]));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "");
+        dataSet.setColor(Color.YELLOW);
+        dataSet.setCircleColor(Color.YELLOW);
+        dataSet.setCircleRadius(4f);
+        dataSet.setLineWidth(2f);
+        dataSet.setDrawFilled(true);
+        dataSet.setFillColor(Color.argb(100, 255, 211, 0));
+        dataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+
+        chart.setDrawGridBackground(false);
+        chart.setDrawBorders(false);
+        chart.getAxisRight().setEnabled(false);
+
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setTextColor(Color.GRAY);
+        yAxis.setAxisMinimum(0);
+        yAxis.setAxisMaximum(100);
+        yAxis.setGranularity(10f);
+        yAxis.setDrawGridLines(true);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(true);
+        xAxis.setTextColor(Color.GRAY);
+        xAxis.setGranularity(1f);
+
+        xAxis.setGranularityEnabled(true);
+        xAxis.setValueFormatter(new com.github.mikephil.charting.formatter.ValueFormatter() {
+            private final String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+            @Override
+            public String getFormattedValue(float value) {
+                int index = (int) value;
+                if (index >= 0 && index < months.length) {
+                    return months[index];
+                } else {
+                    return "";
+                }
+            }
+        });
+
+
+
+        Description desc = new Description();
+        desc.setText("");
+        chart.setDescription(desc);
+        chart.getLegend().setEnabled(false);
+
+        chart.invalidate(); // Refresh chart
 
         return view;
     }
