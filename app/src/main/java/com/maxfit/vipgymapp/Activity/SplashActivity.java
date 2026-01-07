bd40facf5aec10d71a40e7c30d7c36aae400e1d7
gimnakatugampala <gimnakatugampala1@gmail.com>
2026-01-07 15:29:59 +0530

    new_update

--- app/src/main/java/com/maxfit/vipgymapp/Activity/SplashActivity.java
+++ app/src/main/java/com/maxfit/vipgymapp/Activity/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.maxfit.vipgymapp.Activity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.maxfit.vipgymapp.R;
 import com.maxfit.vipgymapp.Utils.SessionManager;
 
@@ -25,6 +27,19 @@ public class SplashActivity extends AppCompatActivity {
 
         logoImage = findViewById(R.id.logoImage);
         sessionManager = new SessionManager(this);
+
+        // --- VERSION CHECK LOGIC ---
+        // If Android 12 (API 31) or higher -> Load original High-Res image
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            logoImage.setImageResource(R.drawable.maxfit_logo_sm);
+        }
+        // If Android 11 or lower -> Use Glide to resize and prevent crashing
+        else {
+            Glide.with(this)
+                    .load(R.drawable.maxfit_logo_sm)
+                    .into(logoImage);
+        }
+        // ---------------------------
 
         // Load and start animation
         Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
